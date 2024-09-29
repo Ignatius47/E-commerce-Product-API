03a3c665eec2e3addeef80e2ad62f1de5f1e001d
Ignatius47 <ignatiusx47@gmail.com>
2024-09-29 16:16:36 +0300

    Initial commit

--- ecommerce_api/ecommerce_api/settings.py
+++ ecommerce_api/ecommerce_api/settings.py
@@ -65,6 +65,8 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -138,6 +140,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # For production use
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
